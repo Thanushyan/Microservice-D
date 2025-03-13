ee44a5f83681737134278e25da58a9d8c02a7be6
Thanushyan Balasankar <balasant@oregonstate.edu>
2025-03-12 17:41:40 -0700

    add

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 2931
+
+app.use(cors())
 
 app.get('/weather/:city', async (req, res) => {
     const city = req.params.city
